c0ee692a8ee2abbd9a6f5e5adf76d91712bb21be
Charles Sharpsteen <source@sharpsteen.net>
2009-07-15 15:13:08 -0700

    - Loaded the device on Windows and Linux to see if anything got broken   by recent changes. Since everything still appears to work, I will merge with the master branch.
    - Updated the DESCRIPTION and README files.
    
    - Added a copyright notice to the top of tikzDevice.c and credited
      Valerio Aimale for creating the PicTeX device from which the C code in
    this device is descended.

--- src/tikzDevice.c
+++ src/tikzDevice.c
@@ -1,3 +1,39 @@
+/*
+ *  tikzDevice, (C) 2009 Charlie Sharpsteen and Cameron Bracken
+ *
+ *  A graphics device for R : 
+ *  	A Computer Language for Statistical Data Analysis
+ *
+ *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
+ *  Copyright (C) 2001-8  The R Development Core Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ *
+ *  The C code in this project started as a fork of:
+ *  	A PicTeX Device, (C) 1996 Valerio Aimale
+ *
+ *
+ *  "If I have seen further, it is only by standing on 
+ *   the shoulders of giants." 
+ *
+ *   -I. Newton
+ *  
+*/
+
+/********************************************************************/
+
 /* 
  * Function prototypes are defined in here. Apparently in C
  * it is absolutely necessary for function definitions to appear 
