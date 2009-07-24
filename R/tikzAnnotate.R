312e3a12ff7f7d2e3a5fe45da89256fcfb2243e2
cameronbracken <cameron.bracken@gmail.com>
2009-07-23 23:19:20 -0600

    tikzAnnotation documentation, examples in vignette

--- R/tikzAnnotate.R
+++ R/tikzAnnotate.R
@@ -2,6 +2,9 @@ tikzAnnotate <-
 function (annotation)
 {	
 
+	if(names(dev.cur()) != "tikz output")
+		stop("The active device is not a tikz device, please start a tikz device to use this function. See ?tikz.")
+		
 	.C('tikzAnnotate', as.character(annotation), 
 		as.integer(length(annotation)), PACKAGE='tikzDevice') 
 	
