2cb3bf55727632e7387a67bb463642de7424b6a9
cameronbracken <cameron.bracken@gmail.com>
2010-10-20 13:18:06 -0600

    Removed device flag hasTextUTF8 to FALSE, and things work!!
    - It seems like we can get by without the textUTF8 functions in the device, as long as the regular text functions are general enough to handle all UTF-8 strings
    
    - Still need to do some checking for when to call XeTeX as opposed to regular LaTeX since the usage now is a hack job
    
    - Added a script to demonstrate the UTF-8 capabilities which should be incorporated into the device tests eventually when things are finalized

---
+++ inst/tests/test-utf8.R
@@ -0,0 +1,8 @@
+require(tikzDevice)
+tikz(standAlone=T)
+x <- matrix(intToUtf8(seq(161,,1,400),multiple=T),20)
+plot(1:20,type='n',xlab='',ylab='',axes=F)
+for(i in 1:20)
+    for(j in 1:20)
+        text(i,j,x[i,j])
+dev.off()
