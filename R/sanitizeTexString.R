9c96ef7932d3cbc27e1cef3d6c715b6dfd68a59c
cameronbracken <cameron.bracken@gmail.com>
2010-11-08 01:58:48 -0700

    Updated Rd documentation for unicode support
    Moved anyMultibyteUTF8Chatacters to its own file since it has its own Rd file and it is a general purpose function
    
    Added an error check in sanitizeTeXString providing a mre informative message if the function chokes on a string (need arose when trying to display plotmath expressions witch contained unknown symbols)

--- R/sanitizeTexString.R
+++ R/sanitizeTexString.R
@@ -66,9 +66,10 @@
 sanitizeTexString <- function(string, 
 	strip = getOption('tikzSanitizeCharacters'),
 	replacement = getOption('tikzReplacementCharacters')){
-		
+
 		  #separate the string into a vector of charaters
 		explode <- strsplit(string,'')[[1]]
+		if(is.na(explode)) stop("Unable to sanitize string, you may be trying to pass in an unsupported symbol")
 
 	    # Replace each matching character with its replacement characters
 		for(i in 1:length(explode)){
