211abd83eafdc234e0520bf45140ddbf0c5d3706
cameronbracken <cameron.bracken@gmail.com>
2010-11-08 01:58:48 -0700

    Updated Rd documentation for unicode support
    Moved anyMultibyteUTF8Chatacters to its own file since it has its own Rd file and it is a general purpose function
    
    Added an error check in sanitizeTeXString providing a mre informative message if the function chokes on a string (need arose when trying to display plotmath expressions witch contained unknown symbols)

--- R/deviceUtils.R
+++ R/deviceUtils.R
@@ -159,35 +159,3 @@ setTikzDefaults <- function( overwrite = TRUE ){
   invisible( tikzSetOptions )
 
 }
-
-anyMultibyteUTF8Characters <- function(string, encoding = "UTF-8"){
-
-  # This function checks if any of the characters in the given string
-  # are multibyte unicode charcters (not ASCII)
-  #
-  # The function will assume an input encoding of UTF-8 but will take any
-  # specified encoding into account and will convert from the specified
-  # encoding to UTF-8 before doing any checks
-
-  mb <- FALSE
-
-    # Set the encoding of the string if it is not explicitly set
-  if(Encoding(string) == "unknown")
-    Encoding(string) <- encoding
-
-    # convert the string to UTF-8
-  string <- enc2utf8(string)
-
-    # Check if any of the characters are Multibyte
-  explode <- strsplit(string,'')[[1]]
-  for(i in 1:length(explode)){
-
-    if(length(charToRaw(explode[i])) > 1){
-      mb <- TRUE
-      break
-    }
-  }
-
-  return(mb)
-
-}
