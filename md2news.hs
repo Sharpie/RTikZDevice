d534106c723f19b296b33b81acf4a98f26b735d4
Charlie Sharpsteen <source@sharpsteen.net>
2011-04-26 13:45:41 -0700

    Preliminary Markdown to Rdoc translation
    Some blocks are translated from Markdown into R documentation format. Itemized
    lists are looking fairly good.
    
    Will need some advanced tricks to deal with closing braces for Sections/Subsections.

--- md2news.hs
+++ md2news.hs
@@ -6,7 +6,8 @@ because... well just because.
 WARNING: Prolonged exposure to Haskell can cause your head to explode.
 -}
 
-import System.Environment
+import System.Environment -- For accessing program arguments
+import Data.Maybe         -- For handling values that may or may not be values
 
 import Text.Pandoc
 import Text.Pandoc.Readers.Markdown
@@ -17,9 +18,10 @@ This function takes the result of a Pandoc parser, extracts the contents,
 formats them into Rd strings and returns a list of the results.
 -}
 pandocToRd :: Pandoc -> [String]
-pandocToRd parsed = do
-  let blocks = getBlocks parsed
-  map show blocks
+-- mapMaybe is like a regular functional mapping except it throws out Nothing
+-- values and unpacks Just values.
+pandocToRd parsed = mapMaybe blockToRd (getBlocks parsed)
+
 
 {-
 This function extracts the "block list" from the Pandoc object returned by
@@ -33,6 +35,39 @@ documentation of the pandoc-types package:
 getBlocks :: Pandoc -> [Block]
 getBlocks (Pandoc meta blocks) = blocks
 
+{-
+This function is responsible for possibly formatting each block element into a
+string. Some block types are ignored and so the value Nothing is returned.
+-}
+blockToRd :: Block -> Maybe String
+-- Individual block types
+blockToRd (Plain elements) = Just $ concat $ inlineListToRd elements
+blockToRd (Para elements) = Just $ concat $ inlineListToRd elements
+blockToRd (Header level elements) = Just $ show $ ["Header" ++ show level, concat $ inlineListToRd elements]
+blockToRd (BulletList blocks) = do
+  let makeListItem list = "\n\t\\item{\n\t\t" : list ++ ["\n\t}"]
+  Just $ "\\itemize{" ++ (concat $ map (concat . makeListItem . blockListToRd) blocks) ++ "\n}"
+blockToRd HorizontalRule = Nothing
+blockToRd Null = Nothing
+-- Passed through uninterpreted for now
+blockToRd other = Just $ show other
+
+blockListToRd :: [Block] -> [String]
+blockListToRd blocks = mapMaybe blockToRd blocks
+
+inlineListToRd :: [Inline] -> [String]
+inlineListToRd elements = mapMaybe inlineToRd elements
+
+{-
+This function is responsible for possibly formatting inline elements into a
+string
+-}
+inlineToRd :: Inline -> Maybe String
+inlineToRd (Str string) = Just string
+inlineToRd (Code attr string) = Just $ "\\code{" ++ string ++ "}"
+inlineToRd Space = Just " "
+inlineToRd other = Just $ show other
+
 
 {- Main Script -}
 main :: IO()
@@ -40,5 +75,8 @@ main = do
   input_file <- fmap (!! 0) (getArgs)
   parsed_markdown <- fmap (readMarkdown defaultParserState) (readFile input_file)
   let results = pandocToRd parsed_markdown
+  -- The unlines function joins a list of strings into one big string using
+  -- newlines
   writeFile "NEWS.Rd" $ unlines results
   putStrLn "Output written to NEWS.Rd"
+
