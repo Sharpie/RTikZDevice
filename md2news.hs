c371e6c61e6744201fc12d1faa6e338688257e83
Charlie Sharpsteen <source@sharpsteen.net>
2011-04-25 19:15:39 -0700

    Deconstruct Pandoc results into a list of blocks

--- md2news.hs
+++ md2news.hs
@@ -12,8 +12,21 @@ import Text.Pandoc
 import Text.Pandoc.Readers.Markdown
 
 
+{-
+This function extracts the "block list" from the Pandoc object returned by
+Pandoc readers such as `readMarkdown`.
+
+More information about the structure of the block list can be found in the
+documentation of the pandoc-types package:
+
+  http://hackage.haskell.org/packages/archive/pandoc-types/1.8/doc/html/Text-Pandoc-Definition.html
+-}
+getBlocks :: Pandoc -> [Block]
+getBlocks (Pandoc meta blocks) = blocks
+
 main :: IO()
 main = do
   input_file <- fmap (!! 0) (getArgs)
   parsed_markdown <- fmap (readMarkdown defaultParserState) (readFile input_file)
-  print parsed_markdown
+  mapM putStrLn (map show (getBlocks parsed_markdown))
+  putStrLn "Done."
