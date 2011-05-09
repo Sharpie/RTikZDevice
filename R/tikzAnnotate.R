2d0f3576bb675a9549989d72b07d990cda0135ae
Charlie Sharpsteen <source@sharpsteen.net>
2011-05-09 01:49:34 -0700

    Add grid grobs for TikZ annotations

--- R/tikzAnnotate.R
+++ R/tikzAnnotate.R
@@ -1,35 +1,103 @@
+#' Convert grid coordinates to device coordinates
+#'
+#' This function converts a coordinate pair specifying a location in a
+#' grid \code{\link{viewport}} in grid units to a coordinate pair specifying a
+#' location in device units relative to the lower left corner of the plotting
+#' canvas.
+#'
+#' @param x x coordinate.
+#' @param y y coordinate. If no values are given for \code{x} and \code{y}, the
+#'   location of the lower-left corner of the current viewport will be
+#'   calculated.
+#' @param units Character string indicating the units of \code{x} and \code{y}.
+#'   See the \code{\link{unit}} function for acceptable unit types.
+#'
+#' @return A tuple of coordinates in device units.
+#'
+#' @author Charlie Sharpsteen \email{source@@sharpsteen.net}
+#'
+#' @keywords graphics grid conversion units
+#' @seealso
+#'   \code{\link{unit}}
+#'   \code{\link{viewport}}
+#'   \code{\link{convertX}}
+#'   \code{\link{convertY}}
+#'   \code{\link{current.transform}}
+#'
+#'
+#' @export
+#' @importFrom grid convertX convertY current.transform
+gridToDevice <- function(x = 0, y = 0, units = 'native') {
+  # Converts a coordinate pair from the current viewport to an "absolute
+  # location" measured in device units from the lower left corner. This is done
+  # by first casting to inches in the current viewport and then using the
+  # current.transform() matric to obtain inches in the device canvas.
+  x <- convertX(unit(x, units), unitTo = 'inches', valueOnly = TRUE)
+  y <- convertY(unit(y, units), unitTo = 'inches', valueOnly = TRUE)
+
+  transCoords <- c(x,y,1) %*% current.transform()
+  transCoords <- (transCoords / transCoords[3])
+
+  return(
+    # Finally, cast from inches to device coordinates (which are TeX points for
+    # the tikzDevice)
+    c(
+      grconvertX(transCoords[1], from = 'inches', to = 'device'),
+      grconvertY(transCoords[2], from = 'inches', to = 'device')
+    )
+  )
+
+}
+
+
 #' Add Custom TikZ Code to an Active Device
+#'
 #' This function allows custom (LaTeX) commands to be added to the output of an
 #' active tikzDevice.
 #'
-#' \code{tikzAnnotate()} is intended to allow the drawing of TikZ commands for
+#' \code{tikzAnnotate} is intended to allow the drawing of TikZ commands for
 #' annotating graphics. If you annotate a graphic with a command that needs a
 #' coordinate \strong{you must convert user coordinates to device coordinates
 #' with the \code{\link{grconvertX}} or \code{\link{grconvertY}} function}.
 #'
-#' \code{tikzCoord()} is a wrapper for \code{tikzAnnotate()} that inserts TikZ
+#' \code{tikzCoord} is a wrapper for \code{tikzAnnotate} that inserts TikZ
 #' \code{\\coordinate} commands into the output.  Coordinates are named
 #' locations that may be referred to by other TikZ drawing commands.
 #'
-#' See the TikZ manual for more information.
+#' \code{tikzAnnotateGrob} and \code{tikzCoordGrob} create "graphics objects"
+#' that are suitable for use with the grid graphics system.
+#' \code{tikzCoordGrob} uses \code{\link{gridToDevice}} to translate between
+#' grid coordinates and device coordinates.
+#'
+#' See the TikZ vignette for more information and examples.
 #'
 #' @usage tikzAnnotate(annotation)
 #'   tikzCoord(x, y, name)
+#'   tikzAnnotateGrob(annotation)
+#'   tikzCoordGrob(x, y, name, units)
 #' @param annotation A character vector, one element per line to be added to
 #'   the open tikz device.
 #' @param x numeric, x location for a named coordinate in user coordinates
 #' @param y numeric, y location for a named coordinate in user coordinates
 #' @param name Character string giving a name for the coordinate (as seen by
 #'   TikZ)
+#' @param units Character string specifying the unit system to be used in grid
+#'   graphics. See \code{\link{unit}} for details.
 #' @return Nothing returned.
 #'
 #' @author Cameron Bracken <cameron.bracken@@gmail.com> and Charlie Sharpsteen
 #'   \email{source@@sharpsteen.net}
 #'
 #' @name tikzAnnotate
-#' @aliases tikzAnnotate tikzCoord
-#' @seealso \code{\link{grconvertX}},
-#'   \code{\link{grconvertY}},\code{\link{tikzDevice}}, \code{\link{tikz}}
+#' @aliases tikzAnnotate tikzCoord tikzAnnotateGrob tikzCoordGrob
+#' @seealso
+#'   \code{\link{grconvertX}}
+#'   \code{\link{grconvertY}}
+#'   \code{\link{tikzDevice}}
+#'   \code{\link{tikz}}
+#'   \code{\link{gridToDevice}}
+#'   \code{\link{unit}}
+#'
 #' @keywords device
 #'
 #' @examples
@@ -85,7 +153,10 @@
 #' 	dev.off()
 #' }
 #'
-#' @export tikzAnnotate tikzCoord
+#' @export tikzAnnotate tikzCoord tikzAnnotateGrob tikzCoordGrob
+#' @importFrom grid grob drawDetails
+#' @S3method drawDetails tikz_annotation
+#' @S3method drawDetails tikz_coord
 NULL
 
 tikzAnnotate <-
@@ -126,54 +197,29 @@ function( x, y, name ){
 }
 
 
-#' Convert grid coordinates to device coordinates
-#'
-#' This function converts a coordinate pair specifying a location in a
-#' grid \code{\link{viewport}} in grid units to a coordinate pair specifying a
-#' location in device units relative to the lower left corner of the plotting
-#' canvas.
-#'
-#' @param x x coordinate.
-#' @param y y coordinate. If no values are given for \code{x} and \code{y}, the
-#'   location of the lower-left corner of the current viewport will be
-#'   calculated.
-#' @param units Character string indicating the units of \code{x} and \code{y}.
-#'   See the \code{\link{unit}} function for acceptable unit types.
-#'
-#' @return A tuple of coordinates in device units.
-#'
-#' @author Charlie Sharpsteen \email{source@@sharpsteen.net}
-#'
-#' @keywords graphics grid conversion units
-#' @seealso
-#'   \code{\link{unit}}
-#'   \code{\link{viewport}}
-#'   \code{\link{convertX}}
-#'   \code{\link{convertY}}
-#'   \code{\link{current.transform}}
-#'
-#'
-#' @export
-#' @importFrom grid convertX convertY current.transform
-gridToDevice <- function(x = 0, y = 0, units = 'native') {
-  # Converts a coordinate pair from the current viewport to an "absolute
-  # location" measured in device units from the lower left corner. This is done
-  # by first casting to inches in the current viewport and then using the
-  # current.transform() matric to obtain inches in the device canvas.
-  x <- convertX(unit(x, units), unitTo = 'inches', valueOnly = TRUE)
-  y <- convertY(unit(y, units), unitTo = 'inches', valueOnly = TRUE)
+tikzAnnotateGrob <- function(annotation) {
 
-  transCoords <- c(x,y,1) %*% current.transform()
-  transCoords <- (transCoords / transCoords[3])
-
-  return(
-    # Finally, cast from inches to device coordinates (which are TeX points for
-    # the tikzDevice)
-    c(
-      grconvertX(transCoords[1], from = 'inches', to = 'device'),
-      grconvertY(transCoords[2], from = 'inches', to = 'device')
-    )
-  )
+  grob(annotation = annotation, cl = 'tikz_annotation')
 
 }
 
+tikzCoordGrob <- function(x, y, name, units = 'native') {
+
+  grob(x = x, y = y, coord_name = name, units = units, cl = 'tikz_coord')
+
+}
+
+drawDetails.tikz_annotation <- function(x, recording) {
+
+  tikzAnnotate(x$annotation)
+
+}
+
+drawDetails.tikz_coord <- function(x, recording) {
+
+  coords <- gridToDevice(x$x, x$y, x$units)
+
+  tikzAnnotate(paste('\\coordinate (', x$coord_name, ') at (',
+    coords[1], ',', coords[2], ');', sep=''))
+
+}
