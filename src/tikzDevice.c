81fd8cf31ca8d81c0bfc5a8dd68124a0928dbfa5
Charlie Sharpsteen <source@sharpsteen.net>
2011-12-15 11:40:43 -0800

    Update dates in copyright notice

--- src/tikzDevice.c
+++ src/tikzDevice.c
@@ -1,7 +1,7 @@
 /*
- *  tikzDevice, (C) 2009-2010 Charlie Sharpsteen and Cameron Bracken
+ *  tikzDevice, (C) 2009-2011 Charlie Sharpsteen and Cameron Bracken
  *
- *  A graphics device for R : 
+ *  A graphics device for R :
  *    A Computer Language for Statistical Data Analysis
  *
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
@@ -9,7 +9,7 @@
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
@@ -25,11 +25,11 @@
  *    A PicTeX Device, (C) 1996 Valerio Aimale
  *
  *
- *  "If I have seen further, it is only by standing on 
- *   the shoulders of giants." 
+ *  "If I have seen further, it is only by standing on
+ *   the shoulders of giants."
  *
  *   -I. Newton
- *  
+ *
 */
 
 /********************************************************************/
