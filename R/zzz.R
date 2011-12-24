8d98cf2db03310cb5df5017731a81db0c42317c9
Charlie Sharpsteen <source@sharpsteen.net>
2011-12-23 23:05:13 -0800

    Use helper classes in zzz.R compiler checks
    Replace old code with new class-based checks that are more generic.

--- R/zzz.R
+++ R/zzz.R
@@ -61,15 +61,7 @@ format.ENV_VAR <- function(x, ...) { sprintf('the environment variable: %s', att
 
 .onLoad <-
 function(libname, pkgname) {
-
-  # Perform a search for an executable LaTeX program.
-  # R environment variables and options will be checked
-  # followed by a check to see if pdflatex is executable
-  # on the path. If LaTeX can not be found, loading of
-  # this package will be aborted as a LaTeX compiler is
-  # required in order to determine string metrics.
-  # Ensure package options are set
-
+  # Ensure options are set.
   setTikzDefaults( overwrite = FALSE )
 
   versionInfo <- read.dcf(file.path( libname, pkgname, "DESCRIPTION"))
@@ -81,108 +73,69 @@ function(libname, pkgname) {
 
   packageStartupMessage( paste(versionInfo,collapse='\n') )
 
+
+  # Perform a search for executable TeX compilers. R options, environment
+  # variables and common paths will be checked. If PdfLaTeX can not be found,
+  # loading of this package will be aborted as a LaTeX compiler is required in
+  # order to determine string metrics. Other compilers, such as XeLaTeX, are
+  # optional.
   foundLatex <- FALSE
   foundXelatex <- FALSE
-  checked <- c()
-  checkedXelatex <- c()
 
-  latexTest <- function( pathToTeX, pathDesc ){
+  latexLocs <- list(
+    OPTION('tikzLatex'),
+    ENV_VAR('R_LATEXCMD'),
+    ENV_VAR('R_PDFLATEXCMD'),
+    OPTION('latexcmd'),
+    PATH('pdflatex'),
+    PATH('latex')
+  )
 
-    Sys.setenv("PATH" = Sys.getenv("PATH"))
-    latexPath <<- Sys.which( paste( pathToTeX ) )
+  # Only check for xelatex in the options and the PATH variable since there are
+  # no environment variables for xelatex.
+  xelatexLocs <- list(
+    OPTION('tikzXelatex'),
+    PATH('xelatex')
+  )
 
-    # Check to see if the path leads to an executible
-    if( file.access(latexPath[1], 1) == 0 ){
-      options( tikzLatex=latexPath )
-      options( tikzLatexDefault=latexPath )
-      foundLatex <<- TRUE
-      checked <<- paste( "\nA working LaTeX compiler was found by checking:\n\t",pathDesc,
-        "\n\nGlobal option tikzLatex set to:\n\t",latexPath,'\n',sep='' )
-      return( TRUE )
-    }else{
-      checked[ length(checked)+1 ] <<- pathDesc
-      return( FALSE )
-    }
-
-  }
-
-  xelatexTest <- function( pathToTeX, pathDesc ){
-
-      Sys.setenv("PATH" = Sys.getenv("PATH"))
-      xelatexPath <<- Sys.which( paste( pathToTeX ) )
-
-      # Check to see if the path leads to an executible
-      if( file.access(xelatexPath[1], 1) == 0 ){
-        options( tikzXelatex = xelatexPath )
-        options( tikzXelatexDefault = xelatexPath )
-        foundXelatex <<- TRUE
-        checkedXelatex <<- paste( "\nA working XeLaTeX compiler was found by checking:\n\t",pathDesc,
-          "\n\nGlobal option tikzXelatex set to:\n\t",xelatexPath,'\n',sep='' )
-        return( TRUE )
-      }else{
-        checkedXelatex[ length(checked)+1 ] <<- pathDesc
-        return( FALSE )
-      }
-
-    }
-
-
-  testLocs <- c( ifelse( is.null(getOption('tikzLatex')), "", getOption('tikzLatex') ),
-    Sys.getenv("R_LATEXCMD"),
-    Sys.getenv("R_PDFLATEXCMD"),
-    ifelse( is.null(getOption('latexcmd')), "NULL", getOption('latexcmd') ),
-    'pdflatex',
-    'latex')
-
-
-  testDescs <- c( "A pre-existing value of the global option tikzLatex",
-    "The R environment variable R_LATEXCMD",
-    "The R environment variable R_PDFLATEXCMD",
-    "The global option latexcmd",
-    "The PATH using the command pdflatex",
-    "The PATH using the command latex")
-
-
-    # Only check for xelatex in the options and the PATH variable since there
-    # no environment variables for xelatex
-  xelatexTestLocs <- c(ifelse( is.null(getOption('tikzXelatex')), "", getOption('tikzXelatex') ),
-    "xelatex")
-
-  xelatexTestDescs <- c("A pre-existing value of the global option tikzXelatex",
-    "The PATH using the command xelatex")
-
-  # Non-Windows users are likely to use some derivative of TeX Live.  This next
+  # Non-Windows users are likely to use some derivative of TeX Live. This next
   # test primarily covers the fact that R.app does not include `/usr/texbin` on
   # the search path on OS X.
   if( .Platform[['OS.type']] == 'unix' ){
-
-    testLocs <- c( testLocs, '/usr/texbin/pdflatex' )
-    testDescs <- c( testDescs, 'The directory /usr/texbin for `pdflatex`' ) 
-    xelatexTestLocs <- c( xelatexTestLocs, '/usr/texbin/xelatex' )
-    xelatexTestDescs <- c( xelatexTestDescs, 'The directory /usr/texbin for `xelatex`' )
-
+    # Using explicit list insertion because the `c` function drops attributes
+    # and thus destroys S3 objects. Writing 3 new class methods for PATH,
+    # OBJECT and ENV_VAR is just overkill.
+    latexLocs[[length(latexLocs) + 1]] <- PATH('/usr/texbin/pdflatex')
+    xelatexLocs[[length(xelatexLocs) + 1]] <- PATH('/usr/texbin/xelatex')
   }
 
-  for( i in 1:length(testLocs) ){
-    if( latexTest( testLocs[i], testDescs[i] ) ){ break }
+  for ( latexPath in latexLocs ) {
+    if ( isExecutable(latexPath) ) {
+      foundLatex <- TRUE
+      options(tikzLatex = as.character(latexPath), tikzLatexDefault = as.character(latexPath))
+      break
+    }
   }
 
-  for( i in 1:length(xelatexTestLocs) ){
-    if( xelatexTest(xelatexTestLocs[i], xelatexTestDescs[i]) ){ break }
+  for( xelatexPath in xelatexLocs ) {
+    if( isExecutable(xelatexPath) ) {
+      foundXelatex <- TRUE
+      options(tikzXelatex = as.character(xelatexPath), tikzXelatexDefault = as.character(xelatexPath))
+      break
+    }
   }
-
 
   if( foundLatex ){
-    packageStartupMessage( checked )
-    p <- pipe( paste( latexPath, '--version' ) )
+    packageStartupMessage( formatExecutable(latexPath) )
+    p <- pipe( paste( as.character(latexPath), '--version' ) )
     packageStartupMessage( paste( utils:::head(readLines( p ), 2), '\n', sep='' ) , sep='\n' )
     close(p)
-  }else{
+  } else {
     stop("\n\nAn appropriate LaTeX compiler could not be found.\n",
       "Access to LaTeX is required in order for the TikZ device\n",
       "to produce output.\n\n",
       "The following places were tested for a valid LaTeX compiler:\n\n\t",
-      paste( checked,collapse='\n\t'),
+      paste( sapply(latexLocs, format),collapse='\n\t'),
       "\n\nIf you have a working LaTeX compiler, try one of the\n",
       "following solutions:",
 
@@ -197,22 +150,19 @@ function(libname, pkgname) {
   }
 
   if( foundXelatex ){
-      packageStartupMessage( checkedXelatex )
-      p <- pipe( paste( xelatexPath, '--version' ) )
+      packageStartupMessage( formatExecutable(xelatexPath) )
+      p <- pipe( paste( as.character(xelatexPath), '--version' ) )
       packageStartupMessage( paste( utils:::head(readLines( p ), 2), '\n', sep='' ) , sep='\n' )
       close(p)
-    }else{
-      packageStartupMessage(
-        "\n\nATTENTION: An appropriate XeLaTeX compiler could not be found.\n",
-        "Access to the `xelatex' command is required for the TikZ device \n",
-        "to produce output containing multibyte UTF-8 characters.\n\n",
-        "The following places were tested for a valid XeLaTeX compiler:\n\n\t",
-        paste( checkedXelatex,collapse='\n\t')
-      )
-    }
-
-    # Its not nice to leave things in the user's global environment
-  rm(latexPath, xelatexPath, envir=.GlobalEnv)
+  } else {
+    packageStartupMessage(
+      "\n\nATTENTION: An appropriate XeLaTeX compiler could not be found.\n",
+      "Access to the `xelatex' command is required for the TikZ device \n",
+      "to produce output containing multibyte UTF-8 characters.\n\n",
+      "The following places were tested for a valid XeLaTeX compiler:\n\n\t",
+      paste( sapply(xelatexLocs, format),collapse='\n\t')
+    )
+  }
 }
 
 # Any variables defined in here will be hidden
